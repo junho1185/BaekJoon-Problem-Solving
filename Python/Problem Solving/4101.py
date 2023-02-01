75d87a2f7d73e5d7c238c9cdb69a179a512f38ef
junho1185 <junho1185@naver.com>
2023-02-02 04:37:29 +0900

    I deleted it

--- Python/Problem Solving/4101.py
+++
@@ -1,8 +0,0 @@
-a,b = map(int, input().split())
-while a != 0 and b != 0:
-    if a > b:
-        print("Yes")
-    else:
-        print("No")
-    a,b = map(int, input().split())
-print("this is the change I made")
