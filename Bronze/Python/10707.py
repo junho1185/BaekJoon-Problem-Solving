365fe5c17b79f56e6940a50bc3239cd3f5436a9e
junho <junho1185@gmail.com>
2023-03-05 17:12:06 +0900

    0305 solve

---
+++ Bronze/Python/10707.py
@@ -0,0 +1,17 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+P = int(input())
+
+X = A*P
+
+if P <= C:
+    Y = B
+else:
+    Y = B + (P-C)*D
+
+if X < Y:
+    print(X)
+else:
+    print(Y)
