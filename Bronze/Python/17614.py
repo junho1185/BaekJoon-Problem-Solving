e32c6b0c11847712f0aab42b8d10b27bea466fee
junho <junho1185@gmail.com>
2023-04-14 20:20:49 +0900

    0414 solve

--- Bronze/Python/17614.py
+++ Bronze/Python/17614.py
@@ -3,9 +3,6 @@ n = int(input())
 cnt = 0
 
 for i in range(1, n+1):
-    number = i
-    while number > 0:
-        if number % 10 == 3 or number % 10 == 6 or number % 10 == 9:
-            cnt += 1
-        number = number // 10
+    number = str(i)
+    cnt += number.count('3') + number.count('6') + number.count('9')
 print(cnt)
