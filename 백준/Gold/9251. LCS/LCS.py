019d4e52b0624530aed513d48f8c8d84acac4a1c
Junho <junho1185@gmail.com>
2023-05-07 14:49:36 +0900

    [Gold V] Title: LCS, Time: 352 ms, Memory: 55716 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -8,10 +8,15 @@ def LCSLength(s1, s2):
     m = len(s1)
     n = len(s2)
 
+    # creating table (m+1) x (n+1) size
     table = [[0] * (n+1) for _ in range(m+1)]
 
+    # propagation loop
     for i in range(1, m + 1):
         for j in range(1, n + 1):
+
+            # I remember this kind of thing from my Algorithm course at uni
+            # A professor from India was explaining how this works
             if s1[i - 1] == s2[j - 1]:
                 table[i][j] = table[i - 1][j - 1] + 1
             else:
